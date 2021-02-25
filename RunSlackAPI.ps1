4bdf46c7beb20dbbbc6abfebfd34c260bbd33c00
Kosuke Kamimori <kamimori.kosuke@fixer.co.jp>
2021-02-25 11:24:26 +0900

    Update RunSlackAPI.ps1

--- RunSlackAPI.ps1
+++ RunSlackAPI.ps1
@@ -2,11 +2,19 @@ $token = "xxxx-"
 $channel = ""
 $api_url = "https://slack.com/api/conversations.history"
 $method = "GET"
+$UNIX_EPOCH = Get-Date("1970/1/1 0:0:0 GMT")
 
-Invoke-RestMethod `
+
+$response = `
+(Invoke-RestMethod `
     -Uri $api_url `
     -Method $method `
     -Body @{
         token="$Token";
         channel="$channel"
+        limit=1;
+        oldest=((Get-Date).AddHours(-1) - $UNIX_EPOCH).TotalSeconds;
+        latest=((Get-Date) - $UNIX_EPOCH).TotalSeconds;
     }
+)
+echo $response
